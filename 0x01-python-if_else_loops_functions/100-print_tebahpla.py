b9d75e962525bea2683fbbdab4b219d20f02c1e5
AugustusTonia <gtonia983@gmail.com>
2022-11-30 06:00:57 -0800

    Task 14 advnc

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-P
